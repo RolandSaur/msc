b755ba33eec2ca2a41a5e525e48aac4e2ed4cc39
oldmansaur <saurroland@googlemail.com>
2015-02-25 16:10:52 +0100

    just ran the problem

--- load_profile/lastprofil.R
+++ load_profile/lastprofil.R
@@ -1,18 +1,17 @@
 library(ggplot2)
 library(reshape2)
 
-fd = read.table('Lastprofil_Haushalt.csv',head =TRUE, skip = 4,sep=',')
+fd = read.table('Lastprofil_Haushalt.csv',head =TRUE, skip = 4,sep=',',dec=',')
 Time_steps =seq(from = 1,to = 96, by = 1)
 fd$Time_steps <- Time_steps
 fd$gruppe <- rep(1,96)
 yy = fd$Werktage.2
 # plot(xx,yy,'l')
-plotler = ggplot(data=fd,aes(x= Time_steps, y= Werktage.2,group = gruppe)) +
+plotler = ggplot(data=fd,aes(x= Time_steps, y= 8*4*Werktage.2,group = gruppe)) +
   geom_line() +
   geom_point(size=2) +
-  xlab("Time") +
+  xlab("Time step") +
   ylab("Load") + 
-  scale_y_discrete(breaks = seq(0, 0.2, by = 0.05))+
   ggtitle("Standard Load Profile")
 
 ggsave(plotler,filename = "loadprofile.png")
