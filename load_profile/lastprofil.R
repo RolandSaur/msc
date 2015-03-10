20ba75e68642ca80d8f9c98ba020feedacbfd06d
oldmansaur <saurroland@googlemail.com>
2015-03-10 15:30:51 +0100

    .project 	.pydevproject 	load_profile/.RData 	load_profile/.Rhistory 	load_profile/profile.txt 	src/

--- load_profile/lastprofil.R
+++ load_profile/lastprofil.R
@@ -5,7 +5,7 @@ fd = read.table('Lastprofil_Haushalt.csv',head =TRUE, skip = 4,sep=',',dec=',')
 Time_steps =seq(from = 1,to = 96, by = 1)
 fd$Time_steps <- Time_steps
 fd$gruppe <- rep(1,96)
-yy = fd$Werktage.2
+yy = fd$Werktage.2 * 8 * 4
 # plot(xx,yy,'l')
 plotler = ggplot(data=fd,aes(x= Time_steps, y= 8*4*Werktage.2,group = gruppe)) +
   geom_line() +
