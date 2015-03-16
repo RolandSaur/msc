cc1339631673b3a85551d4a54eeed3e1b76451a4
oldmansaur <saurroland@googlemail.com>
2015-03-16 17:33:48 +0100

    read_profile also writes to loadprofile.csv

--- src/read_profile.R
+++ src/read_profile.R
@@ -39,3 +39,5 @@ E4A_profile <- with(df,tapply(E4A,van,mean))
 E4A_sd <-with(df,tapply(E4A,van,sd))
 #combine all profiles in one data frame
 loadprofile <- data.frame(E1A_profile,E1B_profile,E1C_profile,E2A_profile,E2B_profile,E3A_profile,E3B_profile,E3C_profile,E3D_profile,E4A_profile,E1A_sd,E1B_sd,E1C_sd,E2A_sd,E2B_sd,E3A_sd,E3B_sd,E3C_sd,E3D_sd,E4A_sd)
+
+write.csv(loadprofile,file = "loadprofile.csv")
