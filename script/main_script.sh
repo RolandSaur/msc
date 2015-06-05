0078c2afd44721324497ca7151f5aa1348b7c219
oldmansaur <saurroland@googlemail.com>
2015-06-05 10:54:09 +0200

    fixed the main script

--- script/main_script.sh
+++ script/main_script.sh
@@ -4,12 +4,13 @@ files_folder=$1
 number_of_runs=$2
 pause=$3
 walltime=$4
-
+IFS=','
 files_csv_files="$files_folder/list_csv.txt"
 
 for file in $(cat $files_csv_files)
 	do
 	runname=${file%.*}
 	$script_folder/hpc_array_run.sh $runname "$files_folder/$file" $number_of_runs $walltime
+	#echo $runname
 	sleep $pause
 	done
