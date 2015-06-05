b983c2c63358acc10ecc68e8db2e4c5c9f3b018d
oldmansaur <saurroland@googlemail.com>
2015-06-05 11:03:43 +0200

    bllll

--- script/hpc_array_run.sh
+++ script/hpc_array_run.sh
@@ -10,11 +10,15 @@ walltime=$4
 #jump to home folder and create folder for the output of this specific configuration
 cd $HOME
 main_node_folder="$HOME/$runname"
-mkdir main_node_folder
-stream_output="$runname/streamoutput"
-mkdir $stream_output
+if [ ! -d $main_node_folder]; then 
+	mkdir main_node_folder
+fi
+stream_output="$main_node_folder/streamoutput"
+if [! -d $stream_output];then
+	mkdir $stream_output
+fi
 
 TEMP="/var/tmp"
 RAMDISK="/tmp/ramdisk"
-
-qsub -t 1-$numberruns -N $runname -l nodes=1:ppn=1,mem=100mb,walltime=$walltime -o $stream_output -e $stream_output -v path_csv=$path_csv,runname=$runname,TEMP=$TEMP,RAMDISK=$RAMDISK,main_node_folder=$main_node_folder $script_folder/single_run_on_node.sh 
+echo $walltime
+#qsub -t 1-$numberruns -N $runname -l nodes=1:ppn=1,mem=100mb,walltime=$walltime -o $stream_output -e $stream_output -v path_csv=$path_csv,runname=$runname,TEMP=$TEMP,RAMDISK=$RAMDISK,main_node_folder=$main_node_folder $script_folder/single_run_on_node.sh 
