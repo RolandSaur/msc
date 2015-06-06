38e3d6232d0327cdb9b7a520818ad9489600d888
oldmansaur <saurroland@googlemail.com>
2015-06-06 12:03:55 -0400

    fixed small folder problem

--- script/single_run_on_node.sh
+++ script/single_run_on_node.sh
@@ -5,6 +5,10 @@ script_folder=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #change into temp folder to write to
 cd $TEMP
 tempfolder=$TEMP"/"$runname"_"$PBS_ARRAYID
+#create the tempfolder if it does not exist yet
+if [ ! -d $tempfolder ]; then
+        mkdir $tempfolder
+fi
 #copy the csv file so that the python script can work with it locally
 cp $path_csv $tempfolder
 
