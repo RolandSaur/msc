e61b3789617c12b38b5bc05f84f2fe752c8b6bc2
oldmansaur <saurroland@googlemail.com>
2015-06-11 11:14:29 -0400

    hm

--- r-scripts/exp_a_evaluation.R
+++ r-scripts/exp_a_evaluation.R
@@ -1,12 +1,11 @@
 
 function_average <- function(df) {
         for (k in c(1:100)){
+          
                 if (k ==1){
-                        df2 <- subset(df,df$run_id == k)
-                        averages <- mean(df2$agent6_soc)
+                        averages <- mean(df[[k]]$agent6_soc)
                 }else {
-                        df2 <- subset(df,df$run_id == k)
-                        averages <- c(averages, mean(df2$agent6_soc))
+                        averages <- c(averages, mean(df[[k]]$agent6_soc))
                 }
         }
         hist(averages)
@@ -16,16 +15,13 @@ main_folder <- "/home/saur/Documents/master/output_data/main_node_folder"
 runs <- c(1,2,3)
 for (i in runs) {
         folder <- paste(main_folder,"/exp_a_",i,sep="")
+        data_frames <- list()
         for (k in c(1:100)){
                 filename <- paste(folder,"/exp_a_",i,"_",k,"_output.csv",sep="")
                 print(filename)
-                if (k==1){
-                        data_frame<-read.csv(filename, header = TRUE, sep = ",")
-                }else{
-                        data_frame <- merge(data_frame,read.csv(filename, header = TRUE, sep = ","),all=TRUE,sort=FALSE)
-                }
+                data_frames[[k]] <-read.csv(filename, header = TRUE, sep = ",")
         }
-        assign(paste("data_a_",i),function_average(data_frame))
+        assign(paste("data_a_",i),function_average(data_frames))
 }
 
 
